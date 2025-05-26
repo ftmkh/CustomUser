583e9f495454c66a78a88bf09f6e717960c011ba
FatemeKhalaji <96fkh99@gmail.com>
2025-05-26 11:29:11 +0330

    add non_superuser with cmd

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,14 @@
-#from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
 
 class CustomUserAdmin(UserAdmin):
-    #add_form = CustomUserCreationForm
-    #form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = CustomUser
-    list_display = ("email", "is_staff", "is_active",)
+    list_display = ("email", "is_staff", "is_active","is_superuser",)
     list_filter = ("email", "is_staff", "is_active",)
     fieldsets = (
         (None, {"fields": ("email", "password")}),
--- users/models.py
+++ users/models.py
@@ -13,6 +13,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    #is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
